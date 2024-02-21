3c1cc1f6e173c0ccc134e26a08ee6556519f7f2f
c11SaadKhurshid <saadkhurshid@Saads-MacBook-Pro.local>
2024-02-22 00:15:28 +0500

    moved linux build to buildDarwin folder

---
+++ buildDarwin/Release/.deps/Release/obj.target/audio/audio-napi.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/audio/audio-napi.o := c++ -o Release/obj.target/audio/audio-napi.o ../audio-napi.cc '-DNODE_GYP_MODULE_NAME=audio' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-D__MACOSX_CORE__' '-DBUILDING_NODE_EXTENSION' -I/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node -I/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/src -I/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/deps/openssl/config -I/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/deps/openssl/openssl/include -I/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/deps/uv/include -I/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/deps/zlib -I/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/deps/v8/include -I/usr/local/opt/libwebm -I../.  -O3 -gdwarf-2 -mmacosx-version-min=10.13 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -std=gnu++14 -stdlib=libc++ -fno-rtti -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/audio/audio-napi.o.d.raw   -c
+Release/obj.target/audio/audio-napi.o: ../audio-napi.cc \
+  /Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node/node_api.h \
+  /Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node/js_native_api.h \
+  /Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node/js_native_api_types.h \
+  /Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node/node_api_types.h \
+  .././webm_muxer.h
+../audio-napi.cc:
+/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node/node_api.h:
+/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node/js_native_api.h:
+/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node/js_native_api_types.h:
+/Users/saadkhurshid/Library/Caches/node-gyp/16.20.2/include/node/node_api_types.h:
+.././webm_muxer.h:
